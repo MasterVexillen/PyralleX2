2a661bf15d92eb9603c72476a82790f32fab37db
Neville Yee <neville.yee@alumni.york.ac.uk>
2022-06-20 16:45:38 +0000

    Changed setup script to use setuptools rather than skbuild.

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
-from skbuild import setup
+from setuptools import setup, find_packages
 
-def main():
-    setup(
-        package_dir={"": "src"},
-        packages={"PyralleX2"},
-        install_requires=[],
-        entry_points = {
-            "console_scripts": [
-                "pyrallex2.clear=PyralleX2.src.PyralleX2.main:clear",
-                "pyrallex2.new=PyralleX2.src.PyralleX2.main:new_config",
-                "pyrallex2.validate=PyralleX2.src.PyralleX2.main:validate_config",
-                "pyrallex2.simulate=PyralleX2.src.PyralleX2.main:simulate",
-                "pyrallex2.visualise=PyralleX2.src.PyralleX2.main:viewslice",
-            ]
-        }
-    )
-
-if __name__ == '__main__':
-    main()
+setup(
+    version="2.1a",
+    name="PyralleX2",
+    package_dir={"": "src"},
+    packages=find_packages("PyralleX2"),
+    zip_safe=False,
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "matplotlib",
+        "pandas",
+        "scikit-build",
+        "pyyaml",
+        "memory_profiler",
+        "tqdm",
+        "icecream",
+        "mrcfile",
+        "magicgui",
+        "pyqt5",
+    ],
+    entry_points = {
+        "console_scripts": [
+            "pyrallex2.clear=PyralleX2.src.PyralleX2.main:clear",
+            "pyrallex2.new=PyralleX2.src.PyralleX2.main:new_config",
+            "pyrallex2.validate=PyralleX2.src.PyralleX2.main:validate_config",
+            "pyrallex2.simulate=PyralleX2.src.PyralleX2.main:simulate",
+            "pyrallex2.visualise=PyralleX2.src.PyralleX2.main:viewslice",
+        ]
+    }
+)
