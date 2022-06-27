5797e1d1b418d41dd6640b7f10f1b1107b3bb1be
Neville Yee <neville.yee@alumni.york.ac.uk>
2022-06-27 08:24:14 +0000

    Fixed git conflicts in setup script.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
-<<<<<<< HEAD
 setup(
     version="2.1a",
     name="PyralleX2",
@@ -24,31 +23,11 @@ setup(
     ],
     entry_points = {
         "console_scripts": [
-            "pyrallex2.clear=PyralleX2.src.PyralleX2.main:clear",
-            "pyrallex2.new=PyralleX2.src.PyralleX2.main:new_config",
-            "pyrallex2.validate=PyralleX2.src.PyralleX2.main:validate_config",
-            "pyrallex2.simulate=PyralleX2.src.PyralleX2.main:simulate",
-            "pyrallex2.visualise=PyralleX2.src.PyralleX2.main:viewslice",
+            "pyrallex2.clear=PyralleX2.main:clear",
+            "pyrallex2.new=PyralleX2.main:new_config",
+            "pyrallex2.validate=PyralleX2.main:validate_config",
+            "pyrallex2.simulate=PyralleX2.main:simulate",
+            "pyrallex2.visualise=PyralleX2.main:viewslice",
         ]
     }
 )
-=======
-def main():
-    setup(
-        package_dir={"": "src"},
-        packages={"PyralleX2"},
-        install_requires=[],
-        entry_points = {
-            "console_scripts": [
-                "pyrallex2.clear=PyralleX2.main:clear",
-                "pyrallex2.new=PyralleX2.main:new_config",
-                "pyrallex2.validate=PyralleX2.main:validate_config",
-                "pyrallex2.simulate=PyralleX2.main:simulate",
-                "pyrallex2.visualise=PyralleX2.main:viewslice",
-            ]
-        }
-    )
-
-if __name__ == '__main__':
-    main()
->>>>>>> fa56a72a3ec89b014dd7a1b4769f9bb57574b9f5
